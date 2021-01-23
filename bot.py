5031ad5fad679ddc4e8aa1c83939b94faf02e957
Misgana123 <62887123+Misgana123@users.noreply.github.com>
2021-01-23 04:57:25 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater("1362586323:AAE0GPCKl8xxyp8cbslxezxAaD5BTa1KJCc", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -51,8 +51,8 @@ def main():
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+                          url_path="1362586323:AAE0GPCKl8xxyp8cbslxezxAaD5BTa1KJCc")
+    updater.bot.setWebhook('https://jarsoai.herokuapp.com/' + "1362586323:AAE0GPCKl8xxyp8cbslxezxAaD5BTa1KJCc")
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
